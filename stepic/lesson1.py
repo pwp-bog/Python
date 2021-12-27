fd3f5a358d21bba9aaa60108380c3ecade91e003
bogdan <bogdanzadora2005@gmail.com>
2021-12-28 00:14:46 +0300

    а
    Омагад

--- stepic/lesson1.py
+++ stepic/lesson1.py
@@ -1,9 +1,11 @@
-value_for_start_number2 = int(input())
-value_max_dell = 0
-for i in range(1, value_for_start_number2 + 1):  # Перебор числа в нужных границах
-    for j in range(1, value_for_start_number2 + 1):  # Проверка на деление числа на делители
-        # Если первое число делится на число из проверки на цело, то выполняем следующий код:
-        if i % j == 0:
-            value_max_dell += 1
-    print(i, "+" * value_max_dell, sep="")
-    value_max_dell = 0
+string = input()
+vowels = 0
+consonants = 0
+for i in range(0, len(string)):
+    if string[i] in "ауоыиэяюёеАУОЫИЭЯЮЁЕ":
+        vowels += 1
+    if string[i] in "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ":
+        consonants += 1
+
+print("Количество гласных букв равно", vowels)
+print("Количество согласных букв равно", consonants)
