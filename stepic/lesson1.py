7e43982db3b543e47029a9714e8dc09ef66a3629
bogdan <bogdanzadora2005@gmail.com>
2022-01-02 21:13:59 +0300

    02.01.2021 учимся)

--- stepic/lesson1.py
+++ stepic/lesson1.py
@@ -1,11 +1,5 @@
-string = input()
-vowels = 0
-consonants = 0
-for i in range(0, len(string)):
-    if string[i] in "ауоыиэяюёеАУОЫИЭЯЮЁЕ":
-        vowels += 1
-    if string[i] in "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ":
-        consonants += 1
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
 
-print("Количество гласных букв равно", vowels)
-print("Количество согласных букв равно", consonants)
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
