ac3b2ba24e5ef344ea6b202ffcba55a951e3251a
bogdan <bogdanzadora2005@gmail.com>
2022-01-07 00:16:22 +0300

    google search v2.0

--- stepic/lesson1.py
+++ stepic/lesson1.py
@@ -1,14 +1,43 @@
-n = int(input())
+#   Количество строк
+first_find = int(input())
 
-list = []
+#   Список строк
+list_string = []
 
-for i in range(n):
-    list.append(input())
+#   Заполнение строк
+for i in range(first_find):
+    list_string.append(input())
 
-find_string = input()
+#   Количество запросов
+secondary_find = int(input())
 
-for i in list:
-    if find_string.upper() in i.upper():
-        print(i)
-    else:
-        continue
+#   Список запросов
+list_find = []
+
+#   Заполнение запросов
+for i in range(secondary_find):
+    list_find.append(input())
+
+#   Объявление
+count = 0
+new_list = []
+
+#   Сам цикл
+for fir in list_string:
+    count = 0
+    for sec in list_find:
+        #   Если совпадает запрос с строкой счётчик плюс 1
+        if sec.lower() in fir.lower():
+            count += 1
+
+        '''
+        Если счётчик равен размеру списка с запросами, то обнуление счётчика
+        и добавление в  новый список
+        '''
+        if count == len(list_find):
+            new_list.append(fir)
+        else:
+            continue
+
+
+print(*new_list, sep="\n")
