9000a99bfbd766af7303bd75eff1a4a297a7576d
bogdan <bogdanzadora2005@gmail.com>
2022-01-12 16:59:10 +0300

    Test

--- pytest/test.py
+++ pytest/test.py
@@ -1,6 +1,9 @@
-from getpass import getpass
+list = [0, 0, 1, 2, 3, 4, 5, 5, 6, 7]
 
-username = input("Введите имя пользователя: ")
-password = getpass("Введите пароль: ")
+for i in list:
+    if i in list:
+        print("Есть дубликаты")
 
-print(password)
+a = input()
+if 1 <= 0:
+    print("text")
---
+++ test_library/test keyboard/test.key.py
@@ -0,0 +1,20 @@
+import keyboard as keyb
+from time import sleep
+
+sleep(0.3)
+keyb.press("win")
+keyb.release("win")
+
+
+sleep(0.3)
+keyb.write("Edge", delay=0.01)
+
+sleep(0.3)
+keyb.press("Enter")
+keyb.release("Enter")
+
+sleep(0.5)
+keyb.write("www.vk.com", delay=0.01)
+
+keyb.press("Enter")
+keyb.release("Enter")
