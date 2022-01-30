d28f0bdb243d8fa1a9b9d470f1a20603da401682
pit_with_pizza <bogdanzadora2005@gmail.com>
2022-01-30 21:32:44 +0300

    [upd] add ghoul program)

---
+++ test_library/test keyboard/notepad.py
@@ -0,0 +1,11 @@
+import keyboard as board
+from time import sleep
+
+for i in range(1, 6):
+	sleep(1)
+	print(i)
+	i += 1
+
+for i in range(1000, 1, -7):
+	board.write(str(f"{i} - 7 = {i - 7}"), delay=0.000000000000000000000000000000000000000000000000000000000000000005)
+	board.send("Enter")
