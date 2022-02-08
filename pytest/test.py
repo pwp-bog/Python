99ea5e40480110415585be76743944ba049c6fdc
pit_with_pizza <bogdanzadora2005@gmail.com>
2022-02-09 01:27:34 +0300

    del tabs

--- pytest/test.py
+++ pytest/test.py
@@ -1,6 +1,5 @@
 from cgi import test
 
-
 list = [0, 0, 1, 2, 3, 4, 5, 5, 6, 7]
 
 for i in list:
@@ -9,7 +8,7 @@ for i in list:
 
 a = input()
 if 1 <= 0:
-    print("text")
+	print("text")
 
 test = 3
 
@@ -17,44 +16,6 @@ a = "string very long string"
 
 a = 5
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 a = a + a
 
 a = "text"
-
