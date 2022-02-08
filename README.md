ec93ef44aa33e68d1c6c24a8d5490d65663a598c
pit_with_pizza <bogdanzadora2005@gmail.com>
2022-02-09 00:54:41 +0300

    test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python
- 
+
+## Сколько времени я воркал:
+<a href="https://wakatime.com/badge/github/pit-with-pizza/Python"><img src="https://wakatime.com/badge/github/pit-with-pizza/Python.svg" alt="wakatime"></a>
--- codewars/1.py
+++ codewars/1.py
@@ -1,14 +1,6 @@
-def xo(string):
-    string_o = 0
-    string_x = 0
-    for i in string.upper():
-        if i == "O":
-            string_o += 1
-        if i == "X":
-            string_x += 1
+def xo(s):
+    s = s.lower()
+    return s.count('x') == s.count('o')
 
-    if string_o == string_x:
-        return True
-    else:
-        return False
-
+print(xo("xoxo"))
+print(xo("xoo"))
