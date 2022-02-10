c167bad74444f45a56de5879bc8503ced9af1bc3
pit_with_pizza <bogdanzadora2005@gmail.com>
2022-02-11 02:17:51 +0300

    new codewars code

--- codewars/1.py
+++ codewars/1.py
@@ -2,5 +2,6 @@ def xo(s):
     s = s.lower()
     return s.count('x') == s.count('o')
 
+
 print(xo("xoxo"))
 print(xo("xoo"))
--- codewars/test.py
+++ codewars/test.py
@@ -1,2 +1,58 @@
-a = input()
-print(a.upper())
+def maps(a):
+    return [x * 2 for x in a]
+
+
+
+print(maps([1, 2, 3]))
+
+a = "My name pit-with-pizza and i like pizza."
+# text #
+
+
+
+
+
+
+
+
+
+
+
+"text"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
