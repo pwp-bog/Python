028f9b8680780e57e641d73d1329bcc1a09f2148
pit_with_pizza <bogdanzadora2005@gmail.com>
2022-02-28 02:49:55 +0300

    hz

--- stepic/lesson1.py
+++ stepic/lesson1.py
@@ -1,1 +1,1 @@
-print(*[m**2 for m in map(int, input().split()) if m % 2 == 0 and m**2 % 10 != 4])
+print(int(input()) *-1)
--- stepic/new_test.py
+++ stepic/new_test.py
@@ -1,1 +1,13 @@
-print("test")
+def first_func():
+    print("test")
+
+    a = "Test polygon for VIM"
+
+
+    if a != 2:
+        print("Not ==")
+    else:
+        print("==")
+
+if __name__ == "__main__":
+    first_func()
