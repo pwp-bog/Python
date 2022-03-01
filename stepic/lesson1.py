f7335b0bc5c828ab2984338b99586459a9631c48
pit_with_pizza <bogdanzadora2005@gmail.com>
2022-03-02 02:28:31 +0300

    learn Python

--- stepic/lesson1.py
+++ stepic/lesson1.py
@@ -1,1 +1,10 @@
-print(int(input()) *-1)
+password = bool(input("Введите пароль: "))
+
+while password:
+    print("Loh")
+    password = input("Введите пароль: ")
+    if password == "loh":
+        print("End.")
+        break
+else:
+    print("down")
